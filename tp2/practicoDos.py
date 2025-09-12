ea672e9f0872b9333f534ec967afa1bcd97775c1
Esteban Perez <perezmalinaresteban@gmail.com>
2025-09-12 14:26:17 -0300

    tp2

---
+++ tp2/practicoDos.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import imageio
+import os
+import tkinter as tk
+from tkinter import filedialog
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+IMAGEN_PATH = os.path.join(".\\IPDI-2025-Grupo19\\tp2\\imagenes", "titocaldern-zq4a.png")
+
